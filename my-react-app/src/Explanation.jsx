2c765eb22976c77b6e8305f0e83905fdcfe59c5f
lucas <102007297+lucas-simmons@users.noreply.github.com>
2026-03-24 17:00:35 -0400

    updated explanation UI

--- my-react-app/src/Explanation.jsx
+++ my-react-app/src/Explanation.jsx
@@ -1,13 +1,41 @@
 import React from "react";
+import { Tooltip } from "antd";
 
 export default function Explanation() {
   return (
     <>
-      <div style={{ justifyItems: "center", paddingBottom: "5px" }}>
+      <div
+        style={{
+          justifyItems: "center",
+          paddingBottom: "5px",
+          borderTop: "5px solid #f8cfa8",
+          backgroundColor: "#c25929",
+          background:
+            "linear-gradient(50deg,rgba(194, 89, 41, 1) 15%, rgba(255, 157, 0, 1) 86%)",
+          boxShadow:
+            "0 16px 32px 0 rgba(0, 0, 0, 0.2), 0 24px 80px 0 rgba(0, 0, 0, 0.19)",
+        }}
+      >
         <h1 style={{ paddingBottom: "0px" }}>
-          Average Curvature + Average Speed + Traffic Level
+          <Tooltip title="50% of Score">
+            <span>Average Curvature</span>
+          </Tooltip>{" "}
+          +{" "}
+          <Tooltip title="20% of Score">
+            <span>Average Speed</span>
+          </Tooltip>{" "}
+          +{" "}
+          <Tooltip title="30% of Score">
+            <span>Traffic Level</span>
+          </Tooltip>{" "}
         </h1>
-        <h1>= Safety Score</h1>
+        <h1>
+          <span>= Safety Score</span>
+
+          <Tooltip title="(Traffic Level * 0.3) + (Average Speed * 0.2) + (Average Curvature * 0.5)">
+            <span style={{ fontSize: "15px" }}>ⓘ</span>
+          </Tooltip>
+        </h1>
       </div>
     </>
   );
